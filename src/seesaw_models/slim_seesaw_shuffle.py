f5ad307ddc8c2c84e218cf9e3fada88983b3ec68
limxuanyu127 <limxuanyu127@gmail.com>
2021-02-05 18:39:18 +0800

    slim experiment

--- src/seesaw_models/slim_seesaw_shuffle.py
+++ src/seesaw_models/slim_seesaw_shuffle.py
@@ -340,7 +340,7 @@ class seesaw_shuffleFaceNetv3(Module):
 
         #Slim
         self.slim1 = Slim(64, 32) #I need to achieve 66 after this, so 66/3 = 22
-        self.max_pool1 = nn.MaxPool2d(2,1)
+        # self.max_pool1 = nn.MaxPool2d(2,1)
         #end
 
         self.conv2_dw = Conv_block(64, 64, kernel=(3, 3), stride=(1, 1), padding=(1, 1), groups=64)
@@ -358,12 +358,12 @@ class seesaw_shuffleFaceNetv3(Module):
     
     def forward(self, x):
         out = self.conv1(x)
-        # print('ckpt1', out.shape)
+        print('ckpt1', out.shape)
 
         #Slim
         out = self.slim1(out)
-        out = self.max_pool1(out)
-        # print('ckpt2', out.shape)
+        # out = self.max_pool1(out)
+        print('ckpt2', out.shape)
 
         #end
 
